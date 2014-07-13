4c0890f936979c2210d03ebe55ca3edd1d784f7c
Tom Lingham <tjlingham@gmail.com>
2014-07-13 21:12:07 +1000

    Initial Commit

---
+++ src/TomLingham/Searchy/SearchyServiceProvider.php
@@ -0,0 +1,42 @@
+<?php namespace TomLingham\Searchy;
+
+use Illuminate\Support\ServiceProvider;
+
+class SearchyServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 *
+	 */
+	public function boot()
+	{
+		$this->package('tom-lingham/searchy');
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
