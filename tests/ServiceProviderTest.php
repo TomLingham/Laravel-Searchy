111521cbb61ed8289f177ee05868612865eeca84
Vincent Klaiber <vincentklaiber@gmail.com>
2016-04-30 11:03:37 +0200

    Assert is injectable

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -3,6 +3,7 @@
 namespace TomLingham\Tests\Searchy;
 
 use GrahamCampbell\TestBenchCore\ServiceProviderTrait;
+use TomLingham\Searchy\SearchBuilder;
 
 /**
  * This is the service provider test.
@@ -12,4 +13,9 @@ use GrahamCampbell\TestBenchCore\ServiceProviderTrait;
 class ServiceProviderTest extends AbstractTestCase
 {
     use ServiceProviderTrait;
+
+    public function testSearchBuilderIsInjectable()
+    {
+        $this->assertIsInjectable(SearchBuilder::class);
+    }
 }
