28cedc927d95778639d959f689779522dc8578c3
Mohamed Abo Bdwey <m.bdwey@gmail.com>
2017-05-30 01:42:15 +0200

    add table name before deleted_at

--- src/SearchDrivers/BaseSearchDriver.php
+++ src/SearchDrivers/BaseSearchDriver.php
@@ -121,7 +121,7 @@ abstract class BaseSearchDriver implements SearchDriverInterface
         // If they included withTrashed flag then give them all records including soft deletes
         // Check to ensure the column exists before committing
         if( ! $this->withTrashed && in_array('deleted_at', Schema::getColumnListing($this->table)) )
-            $this->query = $this->query->where('deleted_at', NULL);
+            $this->query = $this->query->where(\DB::raw("`{$this->table}`.`deleted_at`"), NULL);
 
         return $this->query
             ->orderBy($this->relevanceFieldName, 'desc')
